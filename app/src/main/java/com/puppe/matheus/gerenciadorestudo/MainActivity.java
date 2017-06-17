ec95edd0056651fd920cbc7a82d9f70cde4d30d4
Matheus Puppe <Matheus Puppe>
2017-06-17 19:51:08 -0300

    primeira intent

--- app/src/main/java/com/puppe/matheus/gerenciadorestudo/MainActivity.java
+++ app/src/main/java/com/puppe/matheus/gerenciadorestudo/MainActivity.java
@@ -1,7 +1,14 @@
 package com.puppe.matheus.gerenciadorestudo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickChangeScreen(View view) {
+        Intent intent = new Intent(this, ScreenChoiceActivity.class);
+        startActivity(intent);
     }
 }
